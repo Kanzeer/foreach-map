de54e1be589fd2ba04457600b4d13e942906666f
Kanzeer <miguelroaamaral@gmail.com>
2024-04-17 17:11:14 -0400

    questão 1 finalizada

---
+++ questao1.js
@@ -0,0 +1,6 @@
+//Questão 01 - Crie um array com 5 números e usando o método forEach(), crie um algoritmo para adicionar 1 a cada elemento deste array?
+
+const array = [1, 2, 3, 4, 5];
+array.forEach(function(array){
+    console.log(array + 1); //saída: 1, 2, 3, 4, 5
+});
