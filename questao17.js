97eef813b5e68683e0916522c74d2a8d993d753e
Kanzeer <miguelroaamaral@gmail.com>
2024-04-22 16:10:44 -0400

    questão 17 finalizada

---
+++ questao17.js
@@ -0,0 +1,6 @@
+//Questão 17 - Qual é a diferença entre map() e forEach() em termos de manipulação do array original
+/*
+    O map() não manipula diretamente o array original, já que o mesmo cria um novo array
+
+    O forEach() consegue manipular diretamente o array original, pois o mesmo consegue alterar valores sem a necessidade de um novo array
+*/
