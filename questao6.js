5919b87c5b12f2eb41ddf051fa55fb36bc3bd32b
Kanzeer <miguelroaamaral@gmail.com>
2024-04-22 14:35:44 -0400

    questão 6 finalizada

---
+++ questao6.js
@@ -0,0 +1,8 @@
+//Questão 06 – Crie um algoritmo usando o método forEach() para imprimir apenas os elementos pares de um array?
+
+let array = [1, 2, 3, 4, 5]
+let pares = array.forEach(function(num){
+    if (num % 2 == 0){
+        console.log(num)
+    } 
+})
